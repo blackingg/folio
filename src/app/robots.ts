be5dc34568674b1ab79dd2c5b4da7b57d1b5660c
blackingg <odetundemubarak04@gmail.com>
2026-05-15 07:07:25 +0100

    #

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,23 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://www.whoisblxck.xyz/sitemap.xml",
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: [
+          "GPTBot",
+          "ChatGPT-User",
+          "ClaudeBot",
+          "PerplexityBot",
+          "Google-Extended",
+          "OAI-SearchBot",
+        ],
+        allow: "/",
+      },
+    ],
+    sitemap: "https://whoisblxck.xyz",
   };
 }
