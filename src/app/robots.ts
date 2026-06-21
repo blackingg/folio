2ea3fbb027af048f42cbbfe4372d32537096fdb0
blackingg <odetundemubarak04@gmail.com>
2026-06-21 21:52:02 +0100

    update

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,23 +2,10 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-      },
-      {
-        userAgent: [
-          "GPTBot",
-          "ChatGPT-User",
-          "ClaudeBot",
-          "PerplexityBot",
-          "Google-Extended",
-          "OAI-SearchBot",
-        ],
-        allow: "/",
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
     sitemap: "https://www.whoisblxck.xyz/sitemap.xml",
   };
 }
