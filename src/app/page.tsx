24298087b1b6df0802f6b75e71d11f23bd434d6e
blackingg <odetundemubarak04@gmail.com>
2026-07-14 01:52:20 +0100

    Make lone trailing blog card span full width in two-column grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -250,6 +250,7 @@ export default async function Page() {
               <BlurFade
                 key={post.slug}
                 delay={BLUR_FADE_DELAY * 15 + id * 0.05}
+                className="sm:last:odd:col-span-2 lg:last:odd:col-span-1"
               >
                 <BlogCard
                   title={post.title}
